eb5e7b2ea10f02d5296add0d3d41f5bcbdf816e5
infinitWaves <104868269+infinitWaves@users.noreply.github.com>
2022-12-02 21:52:29 +0100

    done

--- lib/b_game_objects/b1_characters/brocoli.dart
+++ lib/b_game_objects/b1_characters/brocoli.dart
@@ -28,9 +28,10 @@ class Brocoli extends Perso with HasGameRef<DiabeteGameSceneBorocoli> {
               DialogModel(
                   isShowDialog: true, dialogMessage: "Je suis en colère"));
         } else if (gameRef.etape3) {
-          gameRef.gameScenesController.gameDialogController
-              .onDialogList(dialogsMission3Part3);
-          gameRef.etape3IsDone = true;
+          gameRef.gameScenesController.gameDialogController.onDialog(
+              DialogModel(
+                  isShowDialog: true,
+                  dialogMessage: "Laissez moi tranquille !"));
         }
         _hasCollided = true;
       }
@@ -65,69 +66,9 @@ class Brocoli extends Perso with HasGameRef<DiabeteGameSceneBorocoli> {
       isShowDialog: true,
       dialogMessage:
           "Vous constatez que le patient est confus et agressif. Il ne souhaite pas communiquer avec vous ce matin. \n"
-          "Peut-être que son fils pourrait vous aider... \n",
+          "\nPeut-être que son fils pourrait vous aider... \n",
       hasNext: false,
       index: 2,
-    )
-  ];
-
-//#####################################
-// AFFICHAGE DE LA BULLE SUR LE FILS
-//#####################################
-
-//
-  final dialogsMission3Part3 = <DialogModel>[
-    DialogModel(
-      isShowDialog: true,
-      dialogMessage:
-          "L’examen clinique révèle que le patient somnole mais qu’il est réveillable lors de la stimulation verbale.\n"
-          "Il n’est orienté ni dans le temps ni dans l’espace. \n"
-          "Cependant, il sait que vous êtes infirmière au CMS et que son fils est présent dans la salle. \n\n"
-          "Ses paramètres vitaux sont les suivants :\n"
-          "TA = 100/60 mmHg, FC= 98 bpm, FR = 22/min, sat = 90%, température = 37.5oC. La glycémie est à 40 mmol/L. \n",
-      hasNext: true,
-      questionTitle: "Question 3, mission 3:",
-      question: "Au vu des résultats de l’examen clinique, que faites-vous ? ",
-      answers: [
-        {
-          "result": true,
-          "answer": "Appel au médecin traitant",
-        },
-        {
-          "result": false,
-          "answer": "Ouverture du sac à dos et injection de 10 UI d’insuline",
-        },
-        {
-          "result": false,
-          "answer": "Ce n’est pas urgent, aucune intervention nécessaire ",
-        }
-      ],
-      index: 0,
-    ),
-    // if false
-    DialogModel(
-      isShowDialog: true,
-      dialogMessage: "Ce n’est pas la bonne réponse.\n"
-          " Vous perdez 1 point sur la barre de qualité des soins. Retentez ! \n",
-      hasNext: true,
-      index: 1,
-    ),
-    // if true
-    DialogModel(
-      isShowDialog: true,
-      dialogMessage: "C’est la bonne réponse ! \n"
-          "Il s’agit d’une urgence, le patient risque un coma hyperosmolaire. \n"
-          "Vous ne pouvez effectivement pas injecter les 10 UI d’insuline car vous n’avez pas de prescription médicale. \n"
-          "Vous gagnez 1 point sur la barre de qualité des soins.\n",
-      hasNext: true,
-      index: 2,
-    ),
-    DialogModel(
-      isShowDialog: true,
-      dialogMessage:
-          "Ouvrez votre sac à dos et rendez-vous dans l’onglet présentant les professionnels de la santé et sélectionnez l’icône correspondant aux urgences. \n",
-      hasNext: false,
-      index: 3,
     )
   ];
 }
